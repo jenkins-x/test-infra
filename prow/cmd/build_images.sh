880c22dc9c77ea26e89f9e0385f137452f16d548
rawlingsj <rawlingsj80@gmail.com>
2019-01-29 23:24:31 +0000

    add a script to build images

---
+++ prow/cmd/build_images.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+TAG=pipeline11
+CMDS="crier deck hook horologium plank pipeline build sinker tide"
+
+## now loop through the above array
+for i in $CMDS
+do
+  echo "building ${i}"
+  pushd ${i}
+    GOOS=linux GOARCH=amd64  go build ./...
+    docker build -t rawlingsj/${i}:$TAG .
+    docker push rawlingsj/${i}:$TAG
+  popd
+done
+
