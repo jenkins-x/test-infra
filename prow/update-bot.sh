202c368c40c5677a2364ad49cbc7fb1a3cd62ace
Andrew Bayer <andrew.bayer@gmail.com>
2019-09-09 11:38:50 -0400

    fix: Fix git info and committer info for updatebot
    Signed-off-by: Andrew Bayer <andrew.bayer@gmail.com>

--- prow/update-bot.sh
+++ prow/update-bot.sh
@@ -5,7 +5,7 @@ set -o nounset
 set -o pipefail
 
 # Get the new version - since we're pushing via Prow's own bazel-based logic, we don't actually know the version ahead
-# of time. So use the same command used in push.sh to get the version.
-new_version="v$(date -u '+%Y%m%d')-$(git describe --tags --always --dirty)"
+# of time. So get the abbreviated commit.
+new_version="v$(date -u '+%Y%m%d')-$(git rev-parse --short HEAD)"
 
 jx step create pr chart --name gcr.io/jenkinsxio/prow --version ${new_version} --repo https://github.com/jenkins-x-charts/prow.git
