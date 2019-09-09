b5961c0daf0d0f6595bd5ba6743164dbe3455d2c
Andrew Bayer <andrew.bayer@gmail.com>
2019-09-09 11:55:32 -0400

    fix: Use regex, not chart to create PR, fix image tag
    Signed-off-by: Andrew Bayer <andrew.bayer@gmail.com>

--- prow/update-bot.sh
+++ prow/update-bot.sh
@@ -6,6 +6,6 @@ set -o pipefail
 
 # Get the new version - since we're pushing via Prow's own bazel-based logic, we don't actually know the version ahead
 # of time. So get the abbreviated commit.
-new_version="v$(date -u '+%Y%m%d')-$(git rev-parse --short HEAD)"
+new_version="v$(date -u '+%Y%m%d')-$(git rev-parse --short=7 HEAD)"
 
-jx step create pr chart --name gcr.io/jenkinsxio/prow --version ${new_version} --repo https://github.com/jenkins-x-charts/prow.git
+jx step create pr regex --regex "(?m)^\s+repository: gcr.io/jenkinsxio/prow\s+tag: (?P<version>.*)" --version ${new_version} --files prow/values.yaml --repo https://github.com/jenkins-x-charts/prow.git
